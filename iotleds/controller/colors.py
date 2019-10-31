e1351705d7136d67be470e2e68bc84effcc41efd
packard-j <packard-j@husky.neu.edu>
2019-10-31 19:27:29 -0400

    Fixed shift function

--- iotleds/controller/colors.py
+++ iotleds/controller/colors.py
@@ -1,7 +1,6 @@
 from iotleds.bridge.message import Message, SolidColor, Cascade, Rainbow
 from neopixel import NeoPixel
 from math import pi, cos
-from time import sleep
 
 
 class Mode:
@@ -36,26 +35,21 @@ class CascadeMode(Mode):
     def __init__(self, pixels: NeoPixel, msg: Cascade):
         super().__init__(pixels)
         self.color = msg.color
-        self.pixel_colors = [(0, 0, 0)] * 750
+        self.pixels.fill((0, 0, 0))
 
     def update(self, msg: Cascade):
         self.color = msg.color
-        sleep(0.1)
 
     def run(self, **kwargs):
         free = kwargs['free']
         while free():
-            self.shift(self.pixels)
-            sleep(0.1)
+            self.shift()
 
-    def shift(self, pixels: NeoPixel):
-        for i in range(749):
-            self.pixel_colors[i+1] = self.pixel_colors[i]
-        self.pixel_colors[0] = self.color
-        for i in range(750):
-            self.pixels[i] = self.pixel_colors[i]
+    def shift(self):
+        for i in range(749, 0, -1):
+            self.pixels[i] = self.pixels[i-1]
+        self.pixels[0] = self.color
         self.pixels.show()
-        print(self.pixel_colors[0:20])
 
 
 CYCLE = 750
